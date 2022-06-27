8d184afd0cc95f4851c5d405bfa019c4953f046b
Richard Fernando Heise Ferreira <rfhf19@inf.ufpr.br>
2022-06-27 15:22:16 -0300

    Tudo certo

--- Makefile
+++ Makefile
@@ -4,11 +4,16 @@ CFLAGS  = -Wall -g
 CC = gcc 
 
 #-----------------------------------------------------------------------------#
-all : teste1
+all : pingpong-tasks2
 
-run: all
-	./teste1
+runPP3: pingpong-tasks3 
+	./pingpong-tasks3 > minha_saida3.txt
 
+runPP2: pingpong-tasks2
+	./pingpong-tasks2 > minha_saida2.txt
+
+pingpong-tasks2 : ppos_core.o
+pingpong-tasks3 : ppos_core.o 
 teste1 : queue.o ppos_core.o
 contexts : contexts.o queue.o
 testafila : testafila.o queue.o
--- ppos_core.c
+++ ppos_core.c
@@ -7,9 +7,23 @@
 #define STACKSIZE 64*1024
 
 task_t* _currTask, _mainTask;
+int _id = 0;
 queue_t* tasks = NULL;
 
 void ppos_init () {
+
+    // Redundante, porém escolhi manter consistência
+    // não criamos uma stack porque a main já possui uma
+    // afinal ela é uma task
+    _mainTask.prev = NULL;
+    _mainTask.next = NULL;
+    _mainTask.preemptable = 0;
+    _mainTask.status = 1;
+    _mainTask.id = 0;
+    getcontext(&_mainTask.context);
+
+    // Atual é a main
+    _currTask = &_mainTask;
 
     /* desativa o buffer da saida padrao (stdout), usado pela função printf */
     setvbuf (stdout, 0, _IONBF, 0) ;
@@ -17,12 +31,14 @@ void ppos_init () {
 
 int task_create (task_t *task, void (*start_func)(void *), void *arg) {
 
+    // Definimos as variáveis da task
     task->prev = NULL;
     task->next = NULL;
     task->preemptable = 1;
     task->status = 1;
-    task->id = queue_size(tasks) + 1;
+    task->id = ++_id;
 
+    // alocamos uma pilha para a task
     char *stack ;
 
     getcontext (&task->context) ;
@@ -42,18 +58,26 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg) {
     }
 
     makecontext (&task->context, (void*)start_func, 1, arg) ;
-    _currTask = task;
-    queue_append((queue_t **) &tasks, (queue_t *)task);
+
+    return task->id;
 }
 
 int task_switch (task_t *task) {
-    swapcontext(&_currTask->context, &task->context);
+    // A task atual precisa ser guardada antes de ser reatribuída
+    // pois não podemos atribuir após o swapcontext devido à forma 
+    // como a função funciona
+
+    ucontext_t* oldTask = &_currTask->context;
     _currTask = task;
+    swapcontext(oldTask, &task->context);
+
+    return 0;
 }
 
 void task_exit (int exit_code) {
     task_switch(&_mainTask);
-    queue_remove((queue_t **) &tasks, (queue_t *)&_mainTask);
+}
 
-    _currTask = &_mainTask;
+int task_id () {
+    return _currTask->id;
 }
